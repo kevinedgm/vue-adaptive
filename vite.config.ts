6d57d31c49b5631cb3eee0a2f2dc6f425a877c5c
KEVIN GARCIA <kevinedgm@gmail.com>
2026-09-23 16:07:02 -0600

    feat: add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueAdaptive',
+      fileName: 'vue-adaptive',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: { vue: 'Vue' },
+      },
+    },
+  },
+})
