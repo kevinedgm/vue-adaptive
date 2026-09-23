18d7e2d50d33309be02aabead241edab8131eb71
KEVIN GARCIA <kevinedgm@gmail.com>
2026-09-23 16:07:25 -0600

    feat: add examples/AppShellExample.vue

---
+++ examples/AppShellExample.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Adaptive, AdaptiveSwitch } from '@kevinedgm/vue-adaptive'
+import BottomNavigation from './BottomNavigation.vue'
+import NavigationRail from './NavigationRail.vue'
+import Sidebar from './Sidebar.vue'
+</script>
+
+<template>
+  <Adaptive preset="native-detail">
+    <div class="app-shell">
+      <AdaptiveSwitch>
+        <template #compact><BottomNavigation /></template>
+        <template #medium><NavigationRail /></template>
+        <template #expanded><Sidebar /></template>
+      </AdaptiveSwitch>
+
+      <main>
+        <RouterView />
+      </main>
+    </div>
+  </Adaptive>
+</template>
