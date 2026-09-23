869712d9183ec6654c7b3bf898b1fa520f623b93
KEVIN GARCIA <kevinedgm@gmail.com>
2026-09-23 16:07:20 -0600

    feat: add src/components/AdaptiveSwitch.vue

---
+++ src/components/AdaptiveSwitch.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useAdaptive } from '../composables/useAdaptive'
+
+const { size, policy, width } = useAdaptive()
+</script>
+
+<template>
+  <slot
+    v-if="size === 'compact'"
+    name="compact"
+    :size="size"
+    :policy="policy"
+    :width="width"
+  >
+    <slot :size="size" :policy="policy" :width="width" />
+  </slot>
+
+  <slot
+    v-else-if="size === 'medium'"
+    name="medium"
+    :size="size"
+    :policy="policy"
+    :width="width"
+  >
+    <slot :size="size" :policy="policy" :width="width" />
+  </slot>
+
+  <slot
+    v-else
+    name="expanded"
+    :size="size"
+    :policy="policy"
+    :width="width"
+  >
+    <slot :size="size" :policy="policy" :width="width" />
+  </slot>
+</template>
